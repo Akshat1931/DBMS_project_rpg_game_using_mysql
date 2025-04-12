3a40dc649789824aae120c6c2c765107444d46e7
Akshat Baranwal <141770012+Akshat1931@users.noreply.github.com>
2025-04-12 22:58:34 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '@kshat2004',
+  password: 'your_password',
   database: 'game_db'
 });
 
